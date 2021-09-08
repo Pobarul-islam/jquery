5b8b17fd6f5a74a8fc38d5d9046b46b69f270d67
Pobarul <pobarul3@gmail.com>
2021-09-08 20:29:47 +0600

    file added

--- Part 5/js/main.js
+++ Part 5/js/main.js
@@ -1,5 +1,19 @@
+/*$(document).ready(function(){
+
+    $('#hide').click(function(){
+        $('h1').hide(5000);
+    })
+
+    $('#show').click(function(){
+        $('h1').show('slow');
+    })
+
+
+})
+*/
+
 $(document).ready(function(){
-
-$('h1').show();
-
+    $('.toggle').click(function(){
+        $('h3').toggle(1000);
+    })
 })
---
+++ Part 6/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('button').click(function(){
+        $('.box1').fadeToggle(1000);
+        $('.box2').fadeToggle(2000);
+        $('.box3').fadeToggle(3000);
+    })
+})
