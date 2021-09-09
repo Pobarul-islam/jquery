95ff625646d6f7d307deed138de015d3196665d2
Pobarul <pobarul3@gmail.com>
2021-09-09 19:17:28 +0600

    file practised

---
+++ Part 10/js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	$('button').click(function(){
+		$('h5').toggle('slow')
+		alert ('Pobarul isam')
+	})
+})
--- Part 8/js/main.js
+++ Part 8/js/main.js
@@ -1,5 +1,30 @@
 $(document).ready(function(){
     $('button').click(function(){
-        $('div').animate({right:'300px'});
-    })
-})
+      
+      /*  $('div').animate({
+
+         width: 'toggle'
+   })
+  */
+ 
+ /*  
+   var abul = $("div");
+   abul.animate({height: '300px', opacity: '0.5'}, 'slow');
+   abul.animate({width: '300px', opacity: '0.7'}, 'slow');
+   abul.animate({height: '100px', opacity: '0.4'}, 'slow');
+   abul.animate({width: '100px', opacity: '1'}, 'slow');
+
+ */
+
+   var pobarul =$("div");
+   pobarul.animate({left:'300px'},'slow');
+   pobarul.animate({fontSize: '30px'},'fast');
+   pobarul.animate({width:'300px'},'slow');
+
+
+
+
+
+        
+    });
+});
