cd6554da07166c0e954693807ab7f6ddee8b403a
Pobarul <pobarul3@gmail.com>
2021-09-14 15:44:42 +0600

    file added

--- Part 12/js/main.js
+++ Part 12/js/main.js
@@ -1,5 +1,19 @@
 $(document).ready(function(){
-	$('#btn1').click(function(){
-        
-    })
+	
+	$('#button1').click(function(){
+		alert("Babul:"+ $(".text").text())
+	});
+	
+	
+		$('#button2').click(function(){
+		alert("Text:"+ $(".text").html())
+	});
+	
+	$(".butto").click(function(){
+		alert($("#input").val());
+	});
+	
+	$(".button4").click(function(){
+		alert($(".input4").val());
+	})
 })
